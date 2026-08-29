5330d1f6a8e44dee3a440f40701d0872b48b26b4
Rajarshi Roy <rajarshiroy2005456@gmail.com>
2026-08-29 23:04:31 +0530

    backend and frontend stuff

--- backend/src/backend/app/api/config.py
+++ backend/src/backend/app/api/config.py
@@ -1,17 +1,22 @@
+import json
+
 from fastapi import APIRouter
+
+from app.utils.storage import youtube_params_dir
 
 
 router = APIRouter()
 
+config_dir = str(youtube_params_dir() / 'config.json')
+with open(config_dir, "r", encoding="utf-8") as f:
+    config = json.load(f)
 
 @router.get("")
 def get_config():
-    return {
-        "llm": {
-            "provider": "ollama",
-            "model": "qwen3:8b",
-        },
-        "whisper": {
-            "model": "large-v3",
-        },
-    }
+    return config
+
+@router.put("")
+def update_config(new_config: dict):
+    with open(config_dir, "w", encoding="utf-8") as f:
+        json.dump(new_config, f)
+    return new_config
